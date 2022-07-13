cb02d2e24868d868501a4fb2928037ac9a16c02b
Christ <fotsochrist1@gmail.com>
2022-07-13 15:40:06 +0100

    Premiere version du projet

---
+++ script.js
@@ -0,0 +1,19 @@
+window.onscroll = () => {
+
+    if (window.scrollY > 80) {
+        document.querySelector('.header .header-2').classList.add('active');
+    } else {
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+
+}
+
+window.onload = () => {
+
+    if (window.scrollY > 80) {
+        document.querySelector('.header .header-2').classList.add('active');
+    } else {
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+
+}
