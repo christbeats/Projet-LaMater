5ef8b5d046c655d34399664069b20d131be94844
Christ <fotsochrist1@gmail.com>
2022-07-14 12:09:05 +0100

    header, footer, login et register

--- script.js
+++ script.js
@@ -4,6 +4,26 @@ document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
 }
 
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () => {
+    loginForm.classList.remove('active');
+}
+
+let registerForm = document.querySelector('.register-form-container');
+
+document.querySelector('#register-btn').onclick = () => {
+    registerForm.classList.toggle('active');
+}
+
+document.querySelector('#close-register-btn').onclick = () => {
+    registerForm.classList.remove('active');
+}
+
 window.onscroll = () => {
 
     searchForm.classList.remove('active');
